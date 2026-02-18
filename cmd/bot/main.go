78677fb99bebe533a8fe6b5159d9f5952f51b860
Ilya Kuznetsov <chpsdsh@Ilyas-MacBook-Air.local>
2026-02-18 20:27:17 +0700

    hw-1: added /start and /help commands

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,1 +1,31 @@
-package bot
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/application/telegram"
+)
+
+const envFilename = ".env"
+
+func main() {
+	err := godotenv.Load(envFilename)
+	if err != nil {
+		slog.Error("Error loading .env file")
+		os.Exit(1)
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("APP_TELEGRAM_TOKEN"))
+	if err != nil {
+		slog.Error("Error loading .env file")
+		os.Exit(1)
+	}
+
+	bot.Debug = true
+
+	telegramBot := telegram.TelegramBot{Bot: bot}
+	telegramBot.StartMainLoop()
+}
