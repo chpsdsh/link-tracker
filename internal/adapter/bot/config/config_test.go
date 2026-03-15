8efbfe2c5b7b522406158c6dfc057b2517993abe
Ilya Kuznetsov <chpsdsh@Ilyas-MacBook-Air.local>
2026-03-16 00:13:07 +0700

    hw-2: fixed pipeline by fixing test

--- internal/adapter/bot/config/config_test.go
+++ internal/adapter/bot/config/config_test.go
@@ -10,6 +10,7 @@ func TestParseConfig(t *testing.T) {
 		name          string
 		token         string
 		scrapperAddr  string
+		withAPIFlag   string
 		expectedCfg   Config
 		expectedError error
 	}{
@@ -17,9 +18,11 @@ func TestParseConfig(t *testing.T) {
 			name:         "success",
 			token:        "telegram_token",
 			scrapperAddr: "http://localhost:8080",
+			withAPIFlag:  "true",
 			expectedCfg: Config{
 				TelegramToken:         "telegram_token",
 				ScrapperServerAddress: "http://localhost:8080",
+				WithTelegramAPI:       true,
 			},
 			expectedError: nil,
 		},
@@ -27,6 +30,7 @@ func TestParseConfig(t *testing.T) {
 			name:          "missing telegram token",
 			token:         "",
 			scrapperAddr:  "http://localhost:8080",
+			withAPIFlag:   "true",
 			expectedCfg:   Config{},
 			expectedError: ErrNoTelegramToken,
 		},
@@ -37,12 +41,21 @@ func TestParseConfig(t *testing.T) {
 			expectedCfg:   Config{},
 			expectedError: ErrNoScrapperAddress,
 		},
+		{
+			name:          "missing scrapper address",
+			token:         "telegram_token",
+			scrapperAddr:  "http://localhost:8080",
+			withAPIFlag:   "",
+			expectedCfg:   Config{},
+			expectedError: ErrNoTelegramAPIFlag,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Setenv(telegramAPIKey, tt.token)
 			t.Setenv(scrapperServerAddress, tt.scrapperAddr)
+			t.Setenv(withTelegramAPI, tt.withAPIFlag)
 
 			cfg, err := ParseConfig()
 
