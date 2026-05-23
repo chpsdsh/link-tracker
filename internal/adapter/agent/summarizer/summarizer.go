0c46e2aa4246f77254346d1ce8fa6d07bf3e81bf
Ilya Kuznetsov <chpsdsh@Ilyas-MacBook-Air.local>
2026-05-23 12:17:17 +0700

    hw-8: added filtering no summarization yet

--- internal/adapter/agent/summarizer/summarizer.go
+++ internal/adapter/agent/summarizer/summarizer.go
@@ -2,12 +2,18 @@ package summarizer
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/adapter/agent/config"
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/domain/pkg"
 )
 
-var ErrNoDestination = errors.New("no destination to summraize")
+var (
+	ErrTooShortUpdate       = errors.New("update is too shrot")
+	ErrContainsBannedWords  = errors.New("update contains banned words")
+	ErrContainsBannedAuthor = errors.New("update contains banned author")
+)
 
 type LinksSummarizer struct {
 	Config config.AIAgentConfig
@@ -18,11 +24,39 @@ func NewLinksSummarizer(cfg config.AIAgentConfig) LinksSummarizer {
 }
 
 func (s LinksSummarizer) Summarize(update pkg.LinkUpdate) (string, error) { //TODO: переписать на AI API
-	if len(update.Description) == 0 {
-		return "", ErrNoDestination
+	if len(update.Description) < s.Config.MinLength {
+		return "", ErrTooShortUpdate
 	}
+
+	if err := s.checkBannedWords(update); err != nil {
+		return "", fmt.Errorf("banned words: %w", err)
+	}
+
+	if err := s.checkBannedAuthors(update); err != nil {
+		return "", fmt.Errorf("banned authors: %w", err)
+	}
+
 	if len(update.Description) > s.Config.Threshold {
 		return update.Description[:s.Config.Threshold] + "...", nil
 	}
+
 	return update.Description, nil
+}
+
+func (s LinksSummarizer) checkBannedAuthors(update pkg.LinkUpdate) error {
+	for _, author := range s.Config.ExcludedAuthors {
+		if strings.Contains(update.Description, "Автор: "+author) {
+			return fmt.Errorf("%w: %s", ErrContainsBannedWords, author)
+		}
+	}
+	return nil
+}
+
+func (s LinksSummarizer) checkBannedWords(update pkg.LinkUpdate) error {
+	for _, word := range s.Config.StopWords {
+		if strings.Contains(update.Description, word) {
+			return fmt.Errorf("%w: %s", ErrContainsBannedWords, word)
+		}
+	}
+	return nil
 }
